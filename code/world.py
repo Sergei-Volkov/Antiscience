d875741b8594e14571357e511fb67ce9fb6bb0d1
Sergei-Volkov <Sergei-Volkov@users.noreply.github.com>
2020-04-21 00:27:08 +0300

    code upd

--- code/world.py
+++ code/world.py
@@ -1,13 +1,10 @@
 from tqdm import tqdm
 import numpy as np
 def create():
-    for _ in tqdm(range(7), desc='Создание континентов:'):
+    for _ in tqdm(range(7), desc='Создание континентов'):
         pass
-    for _ in tqdm(np.arange(2.7e6), desc='Создание городов:'):
+    for _ in tqdm(np.arange(2.7e6), desc='Создание городов'):
         pass
-    for i in tqdm(np.arange(10e9), desc='Обновление базы данных '
-                                        'пользователей:'):
-        try:
-            i <= 10e7
-        except:
+    for i in tqdm(np.arange(0, 7.5), desc='Обновление базы данных пользователей'):
+        if i <= 5:
             raise ValueError('Ошибка обновления базы данных')
